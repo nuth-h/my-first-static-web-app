026bbca0aaf9e5d241255aaec9637a04d0b2d79c
Nuth <nuth_h@yahoo.com>
2026-03-06 13:41:39 +0700

    Add API: message

---
+++ api/src/functions/message.js
@@ -0,0 +1,17 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+
+
+        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
+
+        /*
+        context.log(`Http function processed request for url "${request.url}"`);
+        const name = request.query.get('name') || await request.text() || 'world';
+        return { body: `Hello, ${name}!` };
+        */
+    }
+});
